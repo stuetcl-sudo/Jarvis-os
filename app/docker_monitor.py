a5f15975089fe5a58c12ae4c1160d9309c10a38e
stuetcl-sudo <stuetcl@gmail.com>
2026-06-29 23:49:54 +0200

    Remove legacy direct Docker start facade

--- app/docker_monitor.py
+++ app/docker_monitor.py
@@ -13,10 +13,3 @@ def classify(name):
 
 def list_containers():
     return _plugin.list_containers()
-
-
-def start_container(name):
-    return _plugin.start_container(name)
-
-
-restart_container = start_container
